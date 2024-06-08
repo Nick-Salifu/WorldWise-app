7a684ecaed310c199f93dc0445431ee70e7642d9
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-06-08 05:05:42 +0100

    Implementing the sidebar and map layout

---
+++ src/components/CityList.jsx
@@ -0,0 +1,13 @@
+import styles from './CityList.module.css'
+
+function CityList() {
+    
+    
+    return (
+        <ul className={styles.cityList}>
+          List of cities
+        </ul>
+    )
+}
+
+export default CityList
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,15 +1,13 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import styles from './PageNav.module.css'
 import Logo from './Logo'
 
 function PageNav() {
     return (
         <nav className={styles.nav}>
-            <Link to="/">
-                <Logo />
-            </Link>
             
-
+            <Logo />
+            
             <ul>
                 <li><NavLink to="/pricing">Pricing</NavLink></li>
                 <li><NavLink to="/product">Product</NavLink></li>
