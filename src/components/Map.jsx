b32f70a0aeace05aa47fe823024016dd8c9e820e
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-06-12 11:45:59 +0100

    adding the position functionality on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 
 function Map() {
@@ -11,25 +11,37 @@ function Map() {
     const [mapPosition, setMapPosition] = useState([40, 0])
 
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
+    const [searchParams] = useSearchParams();
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+
+    useEffect(function () {
+        if(mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    }, [mapLat, mapLng])
 
     return (
         <div className={styles.mapContainer}>
-            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+            <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
+                {cities.map(city => <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                        <span>{city.emoji}</span><span>{city.cityName}</span>
                 </Popup>
-                </Marker>
+                </Marker>)}
+
+                <ChangeCenter position={mapPosition} />
             </MapContainer>   
         </div>
     )
 }
 
-export default Map
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+export default Map;
