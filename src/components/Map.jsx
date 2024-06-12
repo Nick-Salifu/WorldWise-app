2da00c9f862894eaeabd72e9c30f7472d4d80c4e
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-06-13 00:10:47 +0100

    adding more features to the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,30 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 function Map() {
-    const navigate = useNavigate();
     const { cities } = useCities();
-
-    const [mapPosition, setMapPosition] = useState([40, 0])
-
-
-    const [searchParams] = useSearchParams();
-    const mapLat = searchParams.get('lat');
-    const mapLng = searchParams.get('lng');
+    const [mapPosition, setMapPosition] = useState([40, 0]);
+    const {isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation();
+    
+    const [mapLat, mapLng] = useUrlPosition();
 
     useEffect(function () {
-        if(mapLat && mapLng) setMapPosition([mapLat, mapLng]);
-    }, [mapLat, mapLng])
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    }, [mapLat, mapLng]);
+
+    useEffect(function () {
+        if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    }, [geolocationPosition]);
 
     return (
         <div className={styles.mapContainer}>
+           {!geolocationPosition && <Button type='position' onClick={getPosition}>{isLoadingPosition ? 'Loading...' : 'Use your position'}</Button>}
             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -33,6 +37,7 @@ function Map() {
                 </Marker>)}
 
                 <ChangeCenter position={mapPosition} />
+                <DetectClick />
             </MapContainer>   
         </div>
     )
@@ -42,6 +47,13 @@ function ChangeCenter({ position }) {
     const map = useMap();
     map.setView(position);
     return null;
+}
+
+function DetectClick() {
+    const navigate = useNavigate();
+    useMapEvents({
+        click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+    });
 }
 
 export default Map;
