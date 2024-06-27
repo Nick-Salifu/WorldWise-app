7eb7d3d9ebdff64a92bfc1862512e50511fbcc93
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-06-27 01:27:14 +0100

    avatar name change

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -18,7 +18,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-    name: "Jack",
+    name: "Nick",
     email: "jack@example.com",
     password: "qwerty",
     avatar: "https://i.pravatar.cc/100?u=zz",
